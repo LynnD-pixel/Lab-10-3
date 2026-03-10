c5c58b2940bee8b62ed53bc6e3f269d23906592e
Lynn <diamondsforever2016@gmail.com>
2026-03-10 16:32:08 -0400

    updated code to nest providers and wrap app in main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import { FilterProvider } from './context/FilterContext.jsx'
+import { TodoProvider } from './context/TodoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <FilterProvider>
+        <TodoProvider>
+          <App />
+        </TodoProvider>
+      </FilterProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
